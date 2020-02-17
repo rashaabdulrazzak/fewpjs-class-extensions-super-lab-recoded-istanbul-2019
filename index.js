8ce73b2c21fb9d7ccfc3c01a4a22ecadd27e99d7
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-02-17 19:52:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ class Evergreen extends Tree{
     this.name = name 
   }
   static definition(){
-    return (super.definition()+ ``)
+    return (super.definition()+ ` Evergreens keep their leaves all year round`)
   }
 }
